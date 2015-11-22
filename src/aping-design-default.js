0a1881889e60f7f2329e7ae6a5499d7447119fa3
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-11-22 21:31:48 +0100

    added twitter

--- src/aping-design-default.js
+++ src/aping-design-default.js
@@ -9,4 +9,10 @@ angular.module('jtt_aping_design_default', ['linkify', 'angularMoment'])
         $scope.$on('resultMerged', function () {
             $scope.workingCopy = $scope.results;
         });
-    }]);
+    }])
+    .filter('capitalize', function() {
+        return function(input, all) {
+            var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
+            return (!!input) ? input.replace(reg, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
+        }
+    });
